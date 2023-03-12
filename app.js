c1a883811cb3191f9d727b2abb29493f4a10d4eb
スイカ <124490209+opvfbc@users.noreply.github.com>
2023-03-12 11:05:09 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+function solution() {
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    document.getElementById('sum').innerText = +a + +b;
+    }
+
+
+    function solution1() {
+        let a = document.getElementById('a').value;
+        let b = document.getElementById('b').value;
+        document.getElementById('sum').innerText = +a * +b;
+        }
+
+
+        function solution2() {
+            let a = document.getElementById('a').value;
+            let b = document.getElementById('b').value;
+            document.getElementById('sum').innerText = +a / +b;
+            }
+    
+
+            function solution3() {
+                let a = document.getElementById('a').value;
+                let b = document.getElementById('b').value;
+                document.getElementById('sum').innerText = +a - +b;
+                }
+        
